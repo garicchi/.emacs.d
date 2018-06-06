88c76ebc9e9b0a19514da4c25a1820f2bdba8a88
garicchi <xgaricchi@gmail.com>
2018-06-07 08:10:09 +0900

    [fix] replace key

--- init.el
+++ init.el
@@ -229,7 +229,7 @@
 ;; (global-set-key (kbd "C-r") 'yank)
 
 ;; 置換
-(global-set-key (kbd "C-e") 'query-replace)
+(global-set-key (kbd "C-r") 'query-replace)
 
 ;; anything
 (global-set-key (kbd "M-a") 'anything)
