8f3846014a2b50849ed55a6259e79f62e6055601
togai-r <togai-r@klab.com>
2018-06-26 13:55:29 +0900

    fix: refactor

--- init.el
+++ init.el
@@ -165,6 +165,7 @@
 (setq company-idle-delay 0) ; デフォルトは0.5
 (setq company-minimum-prefix-length 2) ; デフォルトは4
 (setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+(add-to-list 'company-backends 'company-edbi)
 
 ;; neotree
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
@@ -173,6 +174,9 @@
 
 ;; flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; sql-mode
+(add-to-list 'auto-mode-alist '("\\.sql?$"     . sql-mode))
 
 ;; web-mode
 (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
