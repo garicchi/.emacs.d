92ab08cbd03e55f88a2ef5b92ef3013df13337e3
togai-r <togai-r@klab.com>
2018-11-23 09:12:41 +0900

    fix:

--- custom.el
+++ custom.el
@@ -9,23 +9,15 @@
     (helm-source-buffers-list helm-source-recentf helm-source-files-in-current-dir helm-source-emacs-commands-history helm-source-emacs-commands)))
  '(package-selected-packages
    (quote
-    (groovy-mode nginx-mode dockerfile-mode yaml-mode yml-mode php-completion php-mode web-mode use-package undo-tree smooth-scroll powerline neotree multi-term monokai-theme markdown-mode json-mode helm flycheck-pos-tip company-go anzu all-the-icons))))
+    (helm-ls-git helm-git-grep company-emacs-eclim eclim groovy-mode nginx-mode dockerfile-mode yaml-mode yml-mode php-completion php-mode web-mode use-package undo-tree smooth-scroll powerline neotree multi-term monokai-theme markdown-mode json-mode helm flycheck-pos-tip company-go anzu all-the-icons))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-builtin-face ((t (:foreground "red" :weight normal))))
- '(font-lock-constant-face ((t (:foreground "magenta"))))
- '(font-lock-function-name-face ((t (:foreground "blue"))))
- '(font-lock-keyword-face ((t (:foreground "red" :weight normal))))
- '(font-lock-string-face ((t (:foreground "yellow"))))
- '(font-lock-type-face ((t (:foreground "green" :slant normal))))
  '(mode-line ((t (:foreground "#f9f9f9" :background "#AD1457" :box nil :height 140))))
  '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil :height 140))))
- '(neo-dir-link-face ((t (:background "#1B1E1C" :foreground "cyan"))))
- '(neo-root-dir-face ((t (:background "#1B1E1C" :foreground "green"))))
  '(powerline-active0 ((t (:foreground "#f9f9f9" :background "#880E4F" :box nil :height 140))))
  '(powerline-active1 ((t (:foreground "#f9f9f9" :background "#666666" :box nil :height 140))))
  '(powerline-active2 ((t (:foreground "#f9f9f9" :background "#AD1457" :box nil :height 140))))
- '(region ((t (:inherit highlight :background "red")))))
+ '(region ((t (:inherit highlight :background "VioletRed3")))))
--- init.el
+++ init.el
@@ -23,17 +23,17 @@
   "Do back to previous tab-stop"
   (interactive)
   (let ((tabs tab-stop-list)
-    (col (current-column))
-    (tab-last 0))
+        (col (current-column))
+        (tab-last 0))
     (back-to-indentation)           ; 現在行の白文字でない最初の文字へポイントを移動
     (if (= col (current-column))        ; 当初のカーソル位置が白文字でない最初の文字位置と一致しているかどうかで期待する位置を調整
-    (while (and tabs (> col (car tabs)))
-      (setq tab-last (car tabs))
-      (setq tabs (cdr tabs)))
+        (while (and tabs (> col (car tabs)))
+          (setq tab-last (car tabs))
+          (setq tabs (cdr tabs)))
       (while (and tabs (>= col (car tabs)))
-      (setq tab-last (car tabs))
-      (setq tabs (cdr tabs))))
-    ; 期待するタブ位置にくるまで１文字ずつ削除
+        (setq tab-last (car tabs))
+        (setq tabs (cdr tabs))))
+                                        ; 期待するタブ位置にくるまで１文字ずつ削除
     (while (> (current-column) tab-last)
       (delete-backward-char 1))))
 
@@ -86,7 +86,7 @@
 ;; はみ出した表示をウインドウの右端で折り返さない
 (setq-default truncate-lines nil)
 (setq-default truncate-partial-width-windows nil)
-        
+
 ;; バッファが外部から変更されたときに自動で再読込
 (global-auto-revert-mode 1)
 
@@ -121,251 +121,16 @@
 ;;(setq w2 (split-window w nil t))
 ;;(setq w3 (split-window w2 nil))
 
-;(ido-mode 1)
-;(ido-everywhere 1)
+                                        ;(ido-mode 1)
+                                        ;(ido-everywhere 1)
 
-;(setq ido-enable-flex-matching t) ;; 中間/あいまい一致
+                                        ;(setq ido-enable-flex-matching t) ;; 中間/あいまい一致
 
 ;; リージョン内を置換するように
 (setq transient-mark-mode t)
 
-;;  ###  パッケージ設定  ###
-;; http://emacs-jp.github.io/packages/package-management/package-el.html
-
-;; ###  パッケージ設定 ###
-
-;; package.elを有効化
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(fset 'package-desc-vers 'package--ac-desc-version)
-(package-initialize)
-
-(when (not (package-installed-p 'use-package))
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
-
-
-(use-package all-the-icons
-             :ensure t)
-(use-package web-mode
-             :ensure t)
-(use-package undo-tree
-             :ensure t)
-(use-package smooth-scroll
-             :ensure t)
-(use-package powerline
-             :ensure t)
-(use-package popup
-             :ensure t)
-(use-package neotree
-             :ensure t)
-(use-package multi-term
-             :ensure t)
-(use-package monokai-theme
-             :ensure t)
-(use-package markdown-mode
-             :ensure t)
-(use-package go-mode
-             :ensure t)
-(use-package flycheck
-             :ensure t)
-(use-package company
-             :ensure t)
-(use-package smooth-scroll
-             :ensure t)
-(use-package anzu
-             :ensure t)
-(use-package helm
-             :ensure t)
-(use-package flycheck-pos-tip
-             :ensure t)
-(use-package company-go
-             :ensure t)
-(use-package json-mode
-  :ensure t)
-(use-package php-mode
-  :ensure t)
-(use-package yaml-mode
-  :ensure t)
-(use-package dockerfile-mode
-  :ensure t)
-(use-package nginx-mode
-  :ensure t)
-(use-package groovy-mode
-  :ensure t)
-
-
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\Jenkinsfile$" . groovy-mode))
-
-
-;; multi-term
-(setq multi-term-program shell-file-name)
-
-;; company-mode
-(global-company-mode) ; 全バッファで有効にする
-(setq company-idle-delay 0) ; デフォルトは0.5
-(setq company-minimum-prefix-length 2) ; デフォルトは4
-(setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
-(add-to-list 'company-backends 'company-edbi)
-(global-set-key (kbd "C-<tab>") 'company-complete)
-
-;; neotree
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-(setq neo-show-hidden-files t)
-(eval-after-load "neotree"
-  '(progn
-     (define-key neotree-mode-map (kbd "C-i") nil)
-     ))
-(neotree)
-
-;flycheck
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
-; flycheck-pos-tip
-(eval-after-load 'flycheck
-  '(custom-set-variables
-   '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
-
-;; sql-mode
-(add-to-list 'auto-mode-alist '("\\.sql?$"     . sql-mode))
-
-;; web-mode
-(add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
-(add-to-list 'auto-mode-alist '("\\.vue?$"     . web-mode))
-
-
-;; powerline
-(custom-set-faces
- '(mode-line ((t (:foreground "#f9f9f9" :background "#AD1457" :box nil :height 140))))
- '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil :height 140))))
- '(powerline-active1 ((t (:foreground "#f9f9f9" :background "#666666" :box nil :height 140))))
- '(powerline-active2 ((t (:foreground "#f9f9f9" :background "#AD1457" :box nil :height 140))))
- '(powerline-active0 ((t (:foreground "#f9f9f9" :background "#880E4F" :box nil :height 140))))
- )
-
-;(powerline-default-theme)
-
-;; undo-tree
-(global-undo-tree-mode)
-
-;; smooth-scroll
-(smooth-scroll-mode t)
-(setq smooth-scroll/vscroll-step-size 8)
-
-;; anzu
-(global-anzu-mode +1)
-
-;; monakai-theme
-(load-theme 'monokai t)
-
-;; helm
-(helm-mode 0)
-(defvar helm-source-emacs-commands
-  (helm-build-sync-source "Emacs commands"
-    :candidates (lambda ()
-                  (let ((cmds))
-                    (mapatoms
-                     (lambda (elt) (when (commandp elt) (push elt cmds))))
-                    cmds))
-    :coerce #'intern-soft
-    :action #'command-execute)
-  "A simple helm source for Emacs commands.")
-
-(defvar helm-source-emacs-commands-history
-  (helm-build-sync-source "Emacs commands history"
-    :candidates (lambda ()
-                  (let ((cmds))
-                    (dolist (elem extended-command-history)
-                      (push (intern elem) cmds))
-                    cmds))
-    :coerce #'intern-soft
-    :action #'command-execute)
-  "Emacs commands history")
-
-(custom-set-variables
- '(helm-mini-default-sources '(helm-source-buffers-list
-                               helm-source-recentf
-                               helm-source-files-in-current-dir
-                               helm-source-emacs-commands-history
-                               helm-source-emacs-commands
-                               )))
-(setq helm-buffers-fuzzy-matching t
-      helm-recentf-fuzzy-match    t)
-
-(define-key global-map (kbd "M-x") 'helm-mini)
-(define-key global-map (kbd "C-x RET") 'helm-mini)
-
 ;; eww
 (setq eww-search-prefix "https://www.google.co.jp/search?btnI&q=")
-
-;;   ###  キーバインド設定 ###
-
-;; ウインドウ移動
-;(global-set-key (kbd "C-M j")  'windmove-left)
-;(global-set-key (kbd "C-M k")  'windmove-down)
-;(Global-Set-Key (Kbd "C-M i")  'windmove-up)
-;(Global-Set-Key (kbd "C-M l") 'windmove-right)
-
-(global-set-key (kbd "C-<left>")  'windmove-left)
-(global-set-key (kbd "C-<down>")  'windmove-down)
-(global-set-key (kbd "C-<up>")    'windmove-up)
-(global-set-key (kbd "C-<right>") 'windmove-right)
-
-
-;; ウインドウ分割
-(global-set-key (kbd "C-d") 'split-window-horizontally)
-(global-set-key (kbd "M-d") 'split-window-vertically)
-(global-set-key (kbd "C-f") 'other-window)
-
-;; 移動
-
-;(global-set-key (kbd "M-:") 'next-line)
-;(global-set-key (kbd "M-@") 'previous-line)
-;(global-set-key (kbd "M-]") 'forward-char)
-;(global-set-key (kbd "M-;") 'backward-char)
-
-
-;; スキップ移動
-
-(global-set-key (kbd "M-<down>") (kbd "C-u 5 <down>"))
-(global-set-key (kbd "M-<up>") (kbd "C-u 5 <up>"))
-(global-set-key (kbd "M-<right>") 'forward-word)
-(global-set-key (kbd "M-<left>") 'backward-word)
-
-;(global-set-key (kbd "M-*") (kbd "C-u 5 <down>"))
-;(global-set-key (kbd "M-`") (kbd "C-u 5 <up>"))
-;(global-set-key (kbd "M-}") 'forward-word)
-;(global-set-key (kbd "M-+") 'backward-word)
-
-;; コピー
-(global-set-key (kbd "C-q") 'copy-region-as-kill)
-(global-set-key (kbd "C-S-w") 'copy-region-as-kill)
-;; (global-set-key (kbd "C-r") 'yank)
-
-;; helm
-(global-set-key (kbd "C-n") 'helm-mini)
-
-
-;; カーソル移動コマンド
-;;(global-set-key "\C-h" 'backward-char)
-;;(global-set-key "\C-j" 'next-line)
-;;(global-set-key "\C-k" 'previous-line)
-;;(global-set-key "\C-l" 'forward-char)
-
-;; バッファリスト
-;; (global-set-key (kbd "M-<tab>") 'switch-to-buffer)
-
-
-;; バッファリストを別ウインドウで開かないようにする
-(global-set-key (kbd "C-x C-b") 'buffer-menu)
-(global-set-key (kbd "C-b") 'buffer-list)
-
-
-;; eww
-(global-set-key (kbd "C-x g") 'eww)
 
 ;; ウインドウをリサイズできるようにする
 (defun window-resizer ()
@@ -397,12 +162,232 @@
                (message "Quit")
                (throw 'end-flag t)))))))
 
+
+
+;;  ###  パッケージ設定  ###
+;; http://emacs-jp.github.io/packages/package-management/package-el.html
+
+;; ###  パッケージ設定 ###
+
+;; package.elを有効化
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(fset 'package-desc-vers 'package--ac-desc-version)
+(package-initialize)
+
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+(use-package all-the-icons
+  :ensure t)
+(use-package web-mode
+  :ensure t)
+(use-package undo-tree
+  :ensure t
+  :config
+  (global-undo-tree-mode)
+  )
+(use-package smooth-scroll
+  :ensure t
+  :config
+  (smooth-scroll-mode t)
+  (setq smooth-scroll/vscroll-step-size 8)
+
+  )
+(use-package powerline
+  :ensure t
+  :config
+  (custom-set-faces
+   '(mode-line ((t (:foreground "#f9f9f9" :background "#AD1457" :box nil :height 140))))
+   '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil :height 140))))
+   '(powerline-active1 ((t (:foreground "#f9f9f9" :background "#666666" :box nil :height 140))))
+   '(powerline-active2 ((t (:foreground "#f9f9f9" :background "#AD1457" :box nil :height 140))))
+   '(powerline-active0 ((t (:foreground "#f9f9f9" :background "#880E4F" :box nil :height 140))))
+   )
+  )
+(use-package popup
+  :ensure t)
+(use-package neotree
+  :ensure t
+  :config
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (setq neo-show-hidden-files t)
+  (eval-after-load "neotree"
+    '(progn
+       (define-key neotree-mode-map (kbd "C-i") nil)
+       ))
+  (global-set-key (kbd "C-c c") 'neotree-create-node)
+  (global-set-key (kbd "C-c d") 'neotree-delete-node)
+  (global-set-key (kbd "C-c r") 'neotree-rename-node)
+  (global-set-key (kbd "C-c p") 'neotree-copy-node)
+  (global-set-key (kbd "C-c s") 'neotree-stretch-toggle)
+  (global-set-key (kbd "C-c t") 'neotree-toggle)
+
+  (neotree)
+  )
+(use-package multi-term
+  :ensure t
+  :config
+  (setq multi-term-program shell-file-name)
+  )
+(use-package monokai-theme
+  :ensure t
+  :config
+  (load-theme 'monokai t)
+  )
+(use-package markdown-mode
+  :ensure t)
+(use-package go-mode
+  :ensure t)
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode)                                        ; flycheck-pos-tip
+  (eval-after-load 'flycheck
+    '(custom-set-variables
+      '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+  )
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode) ; 全バッファで有効にする
+  (setq company-idle-delay 0) ; デフォルトは0.5
+  (setq company-minimum-prefix-length 2) ; デフォルトは4
+  (setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+  (add-to-list 'company-backends 'company-edbi)
+  (global-set-key (kbd "C-<tab>") 'company-complete)
+
+  )
+(use-package smooth-scroll
+  :ensure t)
+(use-package anzu
+  :ensure t
+  :config
+  (global-anzu-mode +1)
+  )
+(use-package helm
+  :ensure t
+  :config
+  (helm-mode 0)
+  (defvar helm-source-emacs-commands
+    (helm-build-sync-source "Emacs commands"
+      :candidates (lambda ()
+                    (let ((cmds))
+                      (mapatoms
+                       (lambda (elt) (when (commandp elt) (push elt cmds))))
+                      cmds))
+      :coerce #'intern-soft
+      :action #'command-execute)
+    "A simple helm source for Emacs commands.")
+
+  (defvar helm-source-emacs-commands-history
+    (helm-build-sync-source "Emacs commands history"
+      :candidates (lambda ()
+                    (let ((cmds))
+                      (dolist (elem extended-command-history)
+                        (push (intern elem) cmds))
+                      cmds))
+      :coerce #'intern-soft
+      :action #'command-execute)
+    "Emacs commands history")
+
+  (custom-set-variables
+   '(helm-mini-default-sources '(helm-source-buffers-list
+                                 helm-source-recentf
+                                 helm-source-files-in-current-dir
+                                 helm-source-emacs-commands-history
+                                 helm-source-emacs-commands
+                                 helm-source-ls-git
+                                 )))
+  (setq helm-buffers-fuzzy-matching t
+        helm-recentf-fuzzy-match    t)
+
+  (define-key global-map (kbd "M-x") 'helm-mini)
+  (define-key global-map (kbd "C-x RET") 'helm-mini)
+
+  )
+(use-package flycheck-pos-tip
+  :ensure t)
+(use-package company-go
+  :ensure t)
+(use-package json-mode
+  :ensure t)
+(use-package php-mode
+  :ensure t)
+(use-package yaml-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+  )
+(use-package dockerfile-mode
+  :ensure t)
+(use-package nginx-mode
+  :ensure t)
+(use-package helm-git-grep
+  :ensure t
+  :config
+  (global-set-key (kbd "C-x g") 'helm-git-grep)
+  (eval-after-load 'helm
+    '(define-key helm-map (kbd "C-x g") 'helm-git-grep-from-helm))
+
+  (global-set-key (kbd "C-x l") 'helm-browse-project)
+
+  )
+(use-package helm-ls-git
+  :ensure t)
+(use-package groovy-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\Jenkinsfile$" . groovy-mode))
+  (defun my-c-mode-hook () 
+    (setq indent-tabs-mode nil 
+          c-basic-offset 4)) 
+  (add-hook 'c-mode-common-hook 'my-c-mode-hook) 
+
+  )
+
+;;   ###  キーバインド設定 ###
+
+;; ウインドウ分割
+(global-set-key (kbd "C-d") 'split-window-horizontally)
+(global-set-key (kbd "M-d") 'split-window-vertically)
+(global-set-key (kbd "C-f") 'other-window)
+
+
+;; スキップ移動
+
+(global-set-key (kbd "M-<down>") (kbd "C-u 5 <down>"))
+(global-set-key (kbd "M-<up>") (kbd "C-u 5 <up>"))
+(global-set-key (kbd "M-<right>") 'forward-word)
+(global-set-key (kbd "M-<left>") 'backward-word)
+
+;; コピー
+(global-set-key (kbd "C-q") 'copy-region-as-kill)
+(global-set-key (kbd "C-S-w") 'copy-region-as-kill)
+
+;; バッファリストを別ウインドウで開かないようにする
+(global-set-key (kbd "C-x C-b") 'buffer-menu)
+(global-set-key (kbd "C-b") 'buffer-list)
+
+;; eww
+(global-set-key (kbd "C-c g") 'eww)
+
+;; window resize
 (global-set-key (kbd "C-x r") 'window-resizer)
 
-;; neotree
-(global-set-key (kbd "C-c d") 'neotree-delete-node)
-(global-set-key (kbd "C-c r") 'neotree-rename-node)
-(global-set-key (kbd "C-c p") 'neotree-copy-node)
+
+;;   ###  ファイルホック ###
+
+;; sql-mode
+(add-to-list 'auto-mode-alist '("\\.sql?$"     . sql-mode))
+
+;; web-mode
+(add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.vue?$"     . web-mode))
+
 
 ;; ### CHEAT COMMAND ###
 (defvar cheat-root "~/.order66/cheat/")
@@ -410,7 +395,7 @@
 (defun cheat()
   (interactive)
   (view-file (concat cheat-root "cheat.md"))
-)
+  )
 
 (defun cheat-tmux()
   (interactive)
