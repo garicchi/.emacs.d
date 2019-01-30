4e5820ecbe654e038db6f9328805b5a14650ba62
togai-r <togai-r@klab.com>
2019-01-31 01:26:41 +0900

    fix: いらないファイル削除

--- custom.el
+++ custom.el
@@ -9,10 +9,10 @@
     (helm-source-buffers-list helm-source-recentf helm-source-ls-git helm-source-files-in-current-dir helm-source-emacs-commands-history helm-source-emacs-commands)))
  '(package-selected-packages
    (quote
-    (spacemacs-theme magit helm-descbinds swiper helm company flycheck go-mode company-glsl glsl-mode plantuml-mode terraform-mode hiwin markdown-preview-mode highlight-symbol yaml-mode web-mode use-package undo-tree tabbar swiper-helm smooth-scroll powerline php-mode nord-theme nginx-mode neotree multi-term monokai-theme markdown-mode json-mode jedi helm-ls-git helm-git-grep helm-ag groovy-mode golden-ratio git-gutter+ flycheck-pos-tip exec-path-from-shell dumb-jump dockerfile-mode dashboard darcula-theme company-jedi company-go company-emacs-eclim color-theme-sanityinc-tomorrow anzu all-the-icons))))
+    (expand-region zenburn-theme yaml-mode web-mode visual-regexp use-package undo-tree terraform-mode tabbar swiper-helm spacemacs-theme smooth-scroll powerline plantuml-mode php-mode nord-theme nginx-mode neotree multiple-cursors multi-term monokai-theme markdown-preview-mode magit json-mode jedi hiwin highlight-symbol helm-ls-git helm-git-grep helm-descbinds helm-ag groovy-mode golden-ratio git-gutter+ flycheck-pos-tip exec-path-from-shell dumb-jump dockerfile-mode dashboard darcula-theme company-jedi company-go company-glsl company-emacs-eclim color-theme-sanityinc-tomorrow anzu all-the-icons))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-variable-name-face ((t (:foreground "#bf9393")))))
+ )
